15372d523bae9c40400fde4a76f0058a1c782768
Aaron Carson <aaron@aaroncarson.co.uk>
2020-11-29 14:05:37 +0000

    autodeploy?

---
+++ ci-deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf $1/tmp
+mkdir $1/tmp
+tar -xzvf $1/tar/build.tar.gz -C $1/tmp
+rsync -Pav --exclude /php/cache --delete $1/tmp/ $1/site
+
+# Thank you to christianspecht.de for this script!
